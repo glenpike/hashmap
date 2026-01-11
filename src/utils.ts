5e0f36fede966025f3f75df6a5b5ead8755d75ec
Glen Pike <glen.pike@olioex.com>
2026-01-11 17:20:06 +0000

    Implementing a hashing function and config tweaking
    Implement a simple Hashing function in JavaScript
    Based on http://www.cse.yorku.ca/~oz/hash.html
    
    Test against an existing library in JavaScript (I didn't look at
    this implementation, promise!)

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
-export function add(a: number, b: number): number {
-  return a + b;
+// http://www.cse.yorku.ca/~oz/hash.html
+export const sdbm = (str: string): number => {
+  let hash = 0
+
+  for(let i = 0; i < str.length;i++) {
+    const c = str.charCodeAt(i)
+    hash = c + (hash << 6) + (hash << 16) - hash;
+  }
+
+  return hash
 }
--- tests/utils_test.ts
+++ tests/utils_test.ts
@@ -1,4 +1,39 @@
-import { add } from '../src/utils';
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
-});
+import { sdbm } from '../src/utils';
+import library_sdbm from 'sdbm';
+
+describe('sdbm hash', () => {
+  it('computes a hash from a single character', () => {
+    expect(sdbm('A')).toEqual('A'.charCodeAt(0))  
+  })
+
+  it('computes a hash from a different single character', () => {
+    expect(sdbm('Z')).toEqual('Z'.charCodeAt(0))  
+  })
+
+  it('computes a hash from a different single number character', () => {
+    expect(sdbm('2')).toEqual('2'.charCodeAt(0))  
+  })
+
+  it('computes a hash from 2 characters', () => {
+    const expected = 4264000
+
+    expect(sdbm('AA')).toEqual(expected)
+  })
+
+  /*
+    https://appzaza.com/sdbm-hasher and https://md5hashing.net/hash/sdbm/4380c473
+    return different results for the string, so rely on a known library implementation
+    to compare our internal function
+  */
+  it('the hash matches the library function', () => {
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    expect(sdbm(str)).toEqual(library_sdbm(str))
+  })
+
+
+  it('computes the hash consistently', () => {
+    const str = 'abcdef'
+    const result = sdbm(str)
+    expect(sdbm(str)).toEqual(result)
+  })
+})
